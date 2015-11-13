f84e6c7bcf120c652ca3876787dc02f853ed8197
ethiraj <esrinivasan_c@rpxcorp.com>
2015-11-13 16:46:29 +0530

    initial commit

---
+++ lib/rails/fort.rb
@@ -0,0 +1,8 @@
+require "rails/fort/version"
+
+module Rails
+  module Fort
+    class Engine < ::Rails::Engine
+    end
+  end
+end
---
+++ lib/rails/fort/version.rb
@@ -0,0 +1,5 @@
+module Rails
+  module Fort
+    VERSION = "0.1.0"
+  end
+end
